286505ffb3bfe43a86a625de52feaaa1012dd891
Jon Jagger <jon@jaggersoft.com>
2017-07-10 18:24:50 +0100

    fixed incomplete red/amber/green regex matching

--- docker/red_amber_green.rb
+++ docker/red_amber_green.rb
@@ -1,6 +1,7 @@
 
 lambda { |stdout,stderr,status|
   output = stdout + stderr
+  return :amber if /(\d+) example(s?), (\d+) failure(s?), (\d+) error(s?)/.match(output)
   return :green if /(\d+) example(s?), 0 failures/.match(output)
   return :red   if /(\d+) example(s?), (\d+) failure(s?)/.match(output)
   return :red   if /(\d+) scenario(s?) \((\d+) failed/.match(output)
