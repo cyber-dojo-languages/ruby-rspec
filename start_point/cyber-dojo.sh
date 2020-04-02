d5fd494d893c3c59d5859e856db65ab590235ef5
Jon Jagger <jon@jaggersoft.com>
2020-04-02 07:48:46 +0100

    Tweak comment layout in cyber-dojo.sh

--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -1,3 +1,4 @@
+# turn off colour in report/coverage.txt
+export NO_COLOR=1
 # Test output can be formatted as progress or documentation
-export NO_COLOR=1 # turn off colour in report/coverage.txt
 rspec . --format progress
