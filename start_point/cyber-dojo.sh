153d2eaa91ef7ddeec93db46098484c71b1defd4
Jon Jagger <jon@jaggersoft.com>
2020-04-03 09:34:27 +0100

    Fix typo in comment

--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -3,7 +3,7 @@ set -e
 # remove existing coverage report
 [ -e report/coverage.txt ] && rm report/coverage.txt
 
-# turn off colour for new coverge report
+# turn off colour for new coverage report
 export NO_COLOR=1
 
 # Test output can be formatted as progress or documentation
